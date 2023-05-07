cb3bd2f241a7c8aeb1f8bea74bc65f8985b3ae90
Badr02 <badrmohammed602@gmail.com>
2023-05-07 20:56:50 +0300

    Adding departments

---
+++ models/department.js
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const department = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    code: {
+        type: String,
+        required: true,
+    }
+});
+
+export default model('department', department);
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import departmentRouter from "./routes/admin/departments.js"
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import mongoose from "mongoose";
-// mongoose.connect(process.env.mongoConnectionUrl);
+import mongoose from "mongoose";
+mongoose.connect(process.env.mongoConnectionUrl);
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true} ));
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
+
+app.use('/admin', departmentRouter);
 
 
 app.listen(process.env.port, () => {
